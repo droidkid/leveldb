c0a448a17917b651a64a59223ad6bdb17d673108
MEDHA KALKUR <u1419499@lab1-17.eng.utah.edu>
2022-12-02 17:23:49 -0700

    Added guess position using PLR

---
+++ hello_ldb.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cassert>
+
+#include "leveldb/db.h"
+
+using namespace std;
+
+#define NUM_KEYS 50000000
+const std::string DB_NAME = "./DB";
+const std::string VALUE = "vaaddd                                                                                dcdwcwcwcewcwecwecwecwecewcewcewdewdwcwecwecwecwecwecwecwecwecwecwecwecfevcggggggggglue::value::value::dljiosdjfskldjfsdlkjfsdlkjfsdlkfjsd";
+
+string generate_key(const string& key) {
+        int key_size=10;
+        string result = string(key_size - key.length(), '0') + key;
+        return std::move(result);
+    }
+int main() {
+    // Destroy the DB and create it again. 
+    // We want to start from scratch.
+    leveldb::Options options;
+    leveldb::Status status = leveldb::DestroyDB(DB_NAME, options);
+    assert(status.ok() || status.IsNotFound());
+
+    leveldb::DB* db;
+    // Probably don't reuse options, safer to create a new one.
+    options.create_if_missing = true;
+    status = leveldb::DB::Open(options, DB_NAME, &db);
+    assert(status.ok());
+
+
+    for (int i=0; i < NUM_KEYS; i++) {
+        string key = generate_key(to_string(i));
+        if(i%1000000==0)std::cout<<"Key: "<<key<<"\n";
+        string *value = new string(VALUE);
+        db->Put(leveldb::WriteOptions(), key, *value);
+        delete value;
+    }
+
+    std::cout<<"DB Stats"<<std::endl;
+    std::string stats;
+    std::cout<<db->GetProperty("leveldb.stats", &stats);
+    std::cout<<stats<<std::endl;
+
+    for (int i=0; i < NUM_KEYS; i++) {
+        string key = generate_key(to_string(i));
+        std::string value;
+        status = db->Get(leveldb::ReadOptions(), key, &value);
+        assert(status.ok() && value == VALUE);
+    }
+
+    cout<<"Ok!"<<endl;
+}
--- table/learned_merger.cc
+++ table/learned_merger.cc
@@ -8,7 +8,9 @@
 #include "leveldb/iterator.h"
 #include "table/iterator_wrapper.h"
 #include "mod/plr.h"
-#include<iostream>
+#include <iostream>
+#include <cmath>
+
 namespace leveldb {
 
 namespace {
@@ -19,29 +21,42 @@ class LearnedMergingIterator : public Iterator {
       : comparator_(comparator),
         children_(new IteratorWrapper[n]),
         keys_data_(std::vector<std::vector<std::string>>()),
-        keys_segments(std::vector<std::vector<Segment>>()),
+        keys_segments_(std::vector<std::vector<Segment>>()),
+        keys_bounds_(std::vector<std::pair<uint64_t, uint64_t>>()),
         n_(n),
         current_(nullptr) {
+    assert(false);
     for (int i = 0; i < n; i++) {
 
       children_[i].Set(children[i]);
       keys_data_.push_back(std::vector<std::string>());
       children_[i].SeekToFirst();
+      // Handle case when children_[i] is empty.
+      uint64_t prev_key = SliceToInteger(children_[i].key());
+      keys_bounds_.push_back(
+        std::pair<uint64_t, uint64_t>(
+                  SliceToInteger(children_[i].key()), 0));
+
       while(children_[i].Valid()) {
         keys_data_[i].push_back(children_[i].key().ToString());
+        keys_bounds_[i].second = (SliceToInteger(children_[i].key()));
         children_[i].Next();
+        
+        uint64_t key = SliceToInteger(children_[i].key());
+        assert(key >= prev_key);
+        prev_key = key;
       }
       children_[i].SeekToFirst();
       PLR plr = PLR(10);   //error=10
       std::vector<Segment> segs = plr.train(keys_data_[i]);
-      keys_segments.push_back(segs);
-      for (auto& str : keys_segments[i]) {
+      keys_segments_.push_back(segs);
+      for (auto& str : keys_segments_[i]) {
         std::cout<<str.x<<" "<<str.k<<" "<<str.b;
       }
       // TODO: train once, instead of every constructor call. For now, we just want something working.        
     }
   }
-
+  
   ~LearnedMergingIterator() override { delete[] children_; }
 
   bool Valid() const override { return (current_ != nullptr); }
@@ -97,6 +112,10 @@ class LearnedMergingIterator : public Iterator {
   void FindSmallest();
   bool GuessPosition(IteratorWrapper* iter, const Slice& guess_key,
                      const Comparator& comparator, std::string& limit);
+  uint64_t GuessPositionFromPLR(
+    const Slice& target_x, 
+    const int iterator_index);
+  uint64_t SliceToInteger(const Slice& slice);
 
   // We might want to use a heap in case there are lots of children.
   // For now we use a simple array since we expect a very small number
@@ -104,13 +123,61 @@ class LearnedMergingIterator : public Iterator {
   const Comparator* comparator_;
   IteratorWrapper* children_;
   std::vector<std::vector<std::string>> keys_data_;
-  std::vector<std::vector<Segment>> keys_segments;
+  std::vector<std::vector<Segment>> keys_segments_;
+  std::vector<std::pair<uint64_t, uint64_t>> keys_bounds_;
   int n_;
   IteratorWrapper* current_;
   // State variables to keep track of current segment.
   std::string limit_;
   bool is_last_segment_;
+
 };
+
+uint64_t LearnedMergingIterator::SliceToInteger(const Slice& slice) {
+    const char* data = slice.data();
+    size_t size = slice.size();
+    uint64_t num = 0;
+    bool leading_zeros = true;
+
+    for (int i = 0; i < size; ++i) {
+        int temp = data[i];
+        if (leading_zeros && temp == '0') continue;
+        leading_zeros = false;
+        num = (num << 3) + (num << 1) + temp - 48;
+    }
+    return num;
+}
+
+
+uint64_t LearnedMergingIterator::GuessPositionFromPLR(
+    const Slice& target_x, 
+    const int iterator_index){
+
+  const std::vector<Segment> segments = keys_segments_[iterator_index];
+  uint64_t min_key = keys_bounds_[iterator_index].first; // TODO: fill the right value
+  uint64_t max_key = keys_bounds_[iterator_index].second; // TODO: fill the right value
+  uint64_t size = keys_data_[iterator_index].size();
+  // check if the key is within the model bounds
+  uint64_t target_int = SliceToInteger(target_x);
+  if (target_int > max_key) return size;
+  if (target_int < min_key) return 0;
+  
+  // binary search between segments
+  uint32_t left = 0, right = (uint32_t)segments.size() - 1;
+  while (left != right - 1) {
+    uint32_t mid = (right + left) / 2;
+    //auto& str = keys_segments[mid];
+    if (target_int < segments[mid].x)
+      right = mid;
+    else
+      left = mid;
+  }
+
+  // calculate the interval according to the selected key segment
+  double result =
+      target_int * segments[left].k + segments[left].b;
+  return floor(result);
+}
 
 bool LearnedMergingIterator::GuessPosition(IteratorWrapper* iter,
                                            const Slice& guess_key,
@@ -136,6 +203,7 @@ bool LearnedMergingIterator::GuessPosition(IteratorWrapper* iter,
 }
 
 void LearnedMergingIterator::FindSmallest() {
+  int smallest_iterator_index = 0;
   IteratorWrapper* smallest = nullptr;
   IteratorWrapper* second_smallest = nullptr;
 
@@ -154,9 +222,11 @@ void LearnedMergingIterator::FindSmallest() {
     if (child->Valid()) {
       if (smallest == nullptr) {
         smallest = child;
+        smallest_iterator_index = i;
       } else if (comparator_->Compare(child->key(), smallest->key()) < 0) {
         second_smallest = smallest;
         smallest = child;
+        smallest_iterator_index = i;
       } else if (second_smallest == nullptr ||
                  comparator_->Compare(child->key(), second_smallest->key()) <
                      0) {
@@ -176,9 +246,13 @@ void LearnedMergingIterator::FindSmallest() {
     is_last_segment_ = true;
     return;
   }
+  // TODO: Guard these with pound signs
   bool hasStrictlyGreaterKey =
       GuessPosition(smallest, second_smallest->key(), *comparator_, limit_);
   is_last_segment_ = !hasStrictlyGreaterKey;
+
+  uint64_t approx_pos = GuessPositionFromPLR(second_smallest->key(),smallest_iterator_index);
+  std::cout<<approx_pos<<"\n";
 }
 }  // namespace
 
